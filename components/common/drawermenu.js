3b1b5bf802496c0697b7b3cc5e8181b5bc72b37d
aliawais0007 <ali.awais5044@gmail.com>
2021-08-11 20:53:52 +0400

    link remove

--- components/common/drawermenu.js
+++ components/common/drawermenu.js
@@ -1,17 +1,12 @@
-import { render } from "react-dom";
 import React from 'react';
-import Image from "next/image";
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from "@material-ui/core/Avatar";
-import Link from "next/Link";
 import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
