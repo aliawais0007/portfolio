93a73fb276c76f7d4ad8fa1d34c22dfdb9dcd850
aliawais0007 <ali.awais5044@gmail.com>
2021-08-22 09:48:04 +0400

    made progressive web app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     loader: "imgix",
     path: "/"
   },
-}
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
